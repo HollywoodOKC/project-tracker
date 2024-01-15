2014e938616b6b14d73708269cf55210a8c74440
HollywoodOKC <T_Lane11@yahoo.com>
2024-01-15 02:36:45 -0600

    feat: Added components.

---
+++ src/components/create.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+export default function Create() {
+    const [form, setForm] = useState({
+        name: "",
+        description: "",
+        progress: "",
+    });
+    const navigate = useNavigate();
+
+    // These methods will update the state properties.
+    function updateForm(value) {
+        return setForm((prev) => {
+            return { ...prev, ...value };
+        });
+    }
+
+    // This function will handle the submission.
+    async function onSubmit(e) {
+        e.preventDefault();
+
+        // When a post request is sent to the create url, we'll add a new record to the database.
+        const newProject = { ...form };
+
+        await fetch("http://localhost:5050/record", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newProject),
+        })
+            .catch(error => {
+                window.alert(error);
+                return;
+            });
+
+        setForm({ name: "", description: "", progress: "" });
+        navigate("/");
+    }
+
+    // This following section will display the form that takes the input from the user.
+    return (
+        <div>
+            <h3>Create New Record</h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label htmlFor="name">Project Name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        value={form.name}
+                        onChange={(e) => updateForm({ name: e.target.value })}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="description">Description</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="description"
+                        value={form.description}
+                        onChange={(e) => updateForm({ description: e.target.value })}
+                    />
+                </div>
+                <div className="form-group">
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="progressOptions"
+                            id="progressOngoing"
+                            value="Ongoing"
+                            checked={form.level === "Ongoing"}
+                            onChange={(e) => updateForm({ progress: e.target.value })}
+                        />
+                        <label htmlFor="positionOngoing" className="form-check-label">Ongoing</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="progressOptions"
+                            id="progressComplete"
+                            value="Complete"
+                            checked={form.level === "Complete"}
+                            onChange={(e) => updateForm({ progress: e.target.value })}
+                        />
+                        <label htmlFor="progressComplete" className="form-check-label">Complete</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="progressOptions"
+                            id="progressR&D"
+                            value="R&D"
+                            checked={form.level === "R&D"}
+                            onChange={(e) => updateForm({ progress: e.target.value })}
+                        />
+                        <label htmlFor="progressR&D" className="form-check-label">Senior</label>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <input
+                        type="submit"
+                        value="Create project"
+                        className="btn btn-primary"
+                    />
+                </div>
+            </form>
+        </div>
+    );
+}
---
+++ src/components/edit.js
@@ -0,0 +1,142 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
+
+export default function Edit() {
+    const [form, setForm] = useState({
+        name: "",
+        position: "",
+        description: "",
+        progress: [],
+    });
+    const params = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function fetchData() {
+            const id = params.id.toString();
+            const response = await fetch(`http://localhost:5050/record/${params.id.toString()}`);
+
+            if (!response.ok) {
+                const message = `An error has occurred: ${response.statusText}`;
+                window.alert(message);
+                return;
+            }
+
+            const record = await response.json();
+            if (!record) {
+                window.alert(`Project with id ${id} not found`);
+                navigate("/");
+                return;
+            }
+
+            setForm(record);
+        }
+
+        fetchData();
+
+        return;
+    }, [params.id, navigate]);
+
+    // These methods will update the state properties.
+    function updateForm(value) {
+        return setForm((prev) => {
+            return { ...prev, ...value };
+        });
+    }
+
+    async function onSubmit(e) {
+        e.preventDefault();
+        const editedProject = {
+            name: form.name,
+            description: form.description,
+            progress: form.progress,
+        };
+
+        // This will send a post request to update the data in the database.
+        await fetch(`http://localhost:5050/record/${params.id}`, {
+            method: "PATCH",
+            body: JSON.stringify(editedProject),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+
+        navigate("/");
+    }
+
+    // This following section will display the form that takes input from the user to update the data.
+    return (
+        <div>
+            <h3>Update Record</h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label htmlFor="name">Project Name: </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        value={form.name}
+                        onChange={(e) => updateForm({ name: e.target.value })}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="description">Description: </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="description"
+                        value={form.description}
+                        onChange={(e) => updateForm({ description: e.target.value })}
+                    />
+                </div>
+                <div className="form-group">
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="progressOptions"
+                            id="progressOngoing"
+                            value="Ongoing"
+                            checked={form.progress === "Ongoing"}
+                            onChange={(e) => updateForm({ progress: e.target.value })}
+                        />
+                        <label htmlFor="progressOutgoing" className="form-check-label">Complete</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="progressOptions"
+                            id="progressComplete"
+                            value="Complete"
+                            checked={form.progress === "Complete"}
+                            onChange={(e) => updateForm({ progress: e.target.value })}
+                        />
+                        <label htmlFor="progressComplete" className="form-check-label">R&D</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name="progressOptions"
+                            id="progressR&D"
+                            value="R&D"
+                            checked={form.progress === "R&D"}
+                            onChange={(e) => updateForm({ progress: e.target.value })}
+                        />
+                        <label htmlFor="progressR&D" className="form-check-label">Senior</label>
+                    </div>
+                </div>
+                <br />
+
+                <div className="form-group">
+                    <input
+                        type="submit"
+                        value="Update Project"
+                        className="btn btn-primary"
+                    />
+                </div>
+            </form>
+        </div>
+    );
+}
