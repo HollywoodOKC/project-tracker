1aec746f67619727be81fb47fb38828b7d78c2ef
HollywoodOKC <T_Lane11@yahoo.com>
2024-01-21 20:52:57 -0600

    refactor: changed name on lines 8 and 18

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Routes } from "react-router-dom";
 
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
-import RecordList from "./components/projectList";
+import ProjectList from "./components/projectList";
 import Edit from "./components/edit";
 import Create from "./components/create";
 
@@ -15,7 +15,7 @@ const App = () => {
             <Navbar />
             <div style={{ margin: 20 }}>
                 <Routes>
-                    <Route exact path="/" element={<RecordList />} />
+                    <Route exact path="/" element={<ProjectList />} />
                     <Route path="/edit/:id" element={<Edit />} />
                     <Route path="/create" element={<Create />} />
                 </Routes>
